9b1eada59802d966bf7adb2df56d491bd6d869a2
shailesh parkhi <shaileshparkhi@imacs-imac.local>
2019-03-27 10:26:05 +0530

    Initial Commit

---
+++ DatabaseManagerSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DatabaseManagerSwift
+//
+//  Created by shailesh parkhi on 3/27/19.
+//  Copyright © 2019 Shailesh Parkhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
