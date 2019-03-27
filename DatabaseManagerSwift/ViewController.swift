011321260f6af6c41b8a4f5dec93e85df8c7497c
shailesh parkhi <shaileshparkhi@imacs-imac.local>
2019-03-27 11:19:33 +0530

    GitHub first commit

--- DatabaseManagerSwift/ViewController.swift
+++ DatabaseManagerSwift/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hello Git")
          print("Branch")
+         print("Git hub first commit")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
