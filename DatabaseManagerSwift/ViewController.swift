9ca29206fe5011a5dba76f0931472039b06151cb
shailesh parkhi <shaileshparkhi@imacs-imac.local>
2019-03-27 10:57:26 +0530

    branch demo

--- DatabaseManagerSwift/ViewController.swift
+++ DatabaseManagerSwift/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello Git")
+         print("Branch")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
